a32aa32f284938cc727235eee428cdb68f735728
Vladislav Sokolov <vlad.sokol099@mail.ru>
2023-01-14 18:11:13 +0300

    working on proxy(block)

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let proxyItemsList = document.querySelectorAll('.proxy__item');
+
+proxyItemsList.forEach(proxyItem => {
+	proxyItem.addEventListener('focus', (event) => {
+		proxyItemsList.forEach(proxyElementClassToRemove => {
+			proxyElementClassToRemove.classList.remove('proxy__item--active')
+		});
+		proxyItem.classList.add('proxy__item--active');
+	});
+});
+
