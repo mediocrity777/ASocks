ecf488c974cd7980532905315cc741ade2eaf154
Vladislav Sokolov <vlad.sokol099@mail.ru>
2023-01-29 21:35:31 +0300

    advantages rewritten

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,4 +3,5 @@ const menuBtn = document.querySelector('.menu-btn');
 
 menuBtn.addEventListener('click', () => {
 	menu.classList.toggle('menu--active');
+	menuBtn.classList.toggle('menu--opened');
 });
